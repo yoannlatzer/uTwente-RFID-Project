2d147e12566744b4dfb79eed2e19690a2c6de039
tavdp <Thymen@wlan225208.mobiel.utwente.nl>
2016-11-03 16:33:03 +0100

    not working yet but fixed errors

--- ToCSV.py
+++ ToCSV.py
@@ -1,3 +1,4 @@
+from eca import *
 import exe_sql as sql
 import datetime
 import urllib.request
@@ -13,11 +14,11 @@ def getcsv():
         while z >= 0:
             csv_data += '{},{},{},{}\r\n'.format(data[z][0], data[z][1], data[z][2], data[z][3])
             z -= 1
-    filename = 'Order_list_{0}.csv'.format(datetime.datetime.now().strftime('%d-%m-%Y:%H:%M:%S'))   #creates a csv file
+    filename = './template_static/csv/Order_list_{0}.csv'.format(datetime.datetime.now().strftime('%d-%m-%Y:%H:%M:%S'))   #creates a csv file
     csv_out = open(filename, 'w')
     csv_out.write(csv_data)     #fills the csv file with csv_data from the orders table
     csv_out.close()             #closes the csv file
-    urllib.request.urlretrieve("%f.csv" % filename, filename)
+    urllib.request.urlretrieve("http://localhost:8080/%s" % filename, filename)
     sql.end()                   #ends the database connection
 
 
